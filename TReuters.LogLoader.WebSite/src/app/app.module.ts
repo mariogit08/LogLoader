8a4d70f3f9ddc89e8d6f525dbde04d067a4de4f5
Mário Augusto Chaves <mario.chaves@live.com>
2021-02-08 02:15:15 -0300

    [MarioChaves] Finally front-end structure and modifications in WebApi about RestFull methods to upload userAgents.

---
+++ TReuters.LogLoader.WebSite/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { LogModule } from './log/log.module';
+import { FormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    LogModule,
+    HttpClientModule,
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
